94740762eaba279bb327a7370f89d7bacf466610
Your Name <moustapha00864@gmail.com>
2022-07-29 08:56:40 +0000

    initial commit

---
+++ employee mgmt PHP/elms/admin/includes/sidebar.php
@@ -0,0 +1,75 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Direction Administrative</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Tableau de Bord</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Departement<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Ajouter Department</a></li>
+                                <li><a href="managedepartments.php">Modifier un Department</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Demandes<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Ajouter une demande</a></li>
+                                <li><a href="manageleavetype.php">Modifier une demande</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employées<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Ajouter un employé</a></li>
+                                <li><a href="manageemployee.php">Modifier un employé</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Gest Demandes<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">Tous les Demandes</a></li>
+                                <li><a href="pending-leavehistory.php">En Attentes...  </a></li>
+                                <li><a href="approvedleave-history.php">Demande Approvée</a></li>
+                                  <li><a href="notapproved-leaves.php">Demande Non Apprové</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Deconecté</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                   <p class="copyright">Mentions Legales © 2022 <a href="https://jokalante.com/"> JOKALANTE </a> Tous Droits Reservés </p>
+                
+                </div>
+                </div>
+            </aside>
